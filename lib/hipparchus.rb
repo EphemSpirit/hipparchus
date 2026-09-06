19397c18896f85e06e00635208f52c02fe3ac20d
Drew Hund <languageinvestigator@gmail.com>
2026-09-06 11:49:14 -0400

    init and gemspec

---
+++ lib/hipparchus.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hipparchus/version"
+
+module Hipparchus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hipparchus/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Hipparchus
+  VERSION = "0.1.0"
+end
